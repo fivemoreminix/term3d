566901be1c5bb63276adc28efb5856f1739ee4c6
Luke Wilson <asmoaesl@gmail.com>
2018-11-08 14:44:01 -0600

    Fixed line rendering

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,66 @@ extern crate easycurses;
 use easycurses::Color::*;
 use easycurses::*;
 
-fn draw_line(easy: &mut EasyCurses, pos1: (i32, i32), pos2: (i32, i32)) {
-    let a = 2 * (pos2.1 - pos1.1); // a = 2 * change in Y
-    let b = a - (2 * (pos2.0 - pos1.0)); // b = a - 2 * change in X
-    let mut p = a - (pos2.0 - pos1.0); // p = a - change in X
+fn draw_cell(e: &mut EasyCurses, c:char,x:i32,y:i32) {
+    e.move_xy(x, y);
+    e.print_char(c);
+}
 
-    easy.move_xy(pos1.0, pos1.1);
-    
-    let mut y = pos1.1;
-    for x in pos1.0..pos2.0 {
-        if p < 0 {
-            p += a;
-        } else /*if p >= 0*/ {
-            y += 1;
-            p += b;
+fn draw_line_low(e: &mut EasyCurses, x0:i32,y0:i32,x1:i32,y1:i32) {
+    let dx = x1 - x0;
+    let mut dy = y1 - y0;
+    let mut yi = 1;
+    if dy < 0 {
+        yi = -1;
+        dy = -dy;
+    }
+    let mut D = 2*dy - dx;
+    let mut y = y0;
+
+    for x in x0..x1 {
+        draw_cell(e, '#', x, y);
+        if D > 0 {
+            y += yi;
+            D -= 2*dx;
         }
+        D += 2*dy;
+    }
+}
 
-        easy.move_xy(x, y);
-        easy.print_char('#');
+fn draw_line_high(e: &mut EasyCurses, x0:i32,y0:i32,x1:i32,y1:i32) {
+    let mut dx = x1 - x0;
+    let dy = y1 - y0;
+    let mut xi = 1;
+    if dx < 0 {
+        xi = -1;
+        dx = -dx;
+    }
+    let mut D = 2*dx - dy;
+    let mut x = x0;
+
+    for y in y0..y1 {
+        draw_cell(e, '#', x, y);
+        if D > 0 {
+            x += xi;
+            D -= 2*dy;
+        }
+        D += 2*dx;
+    }
+}
+
+fn draw_line(e: &mut EasyCurses, x0:i32,y0:i32,x1:i32,y1:i32) {
+    if (y1 - y0).abs() < (x1 - x0).abs() {
+        if x0 > x1 {
+            draw_line_low(e, x1, y1, x0, y0);
+        } else {
+            draw_line_low(e, x0, y0, x1, y1);
+        }
+    } else {
+        if y0 > y1 {
+            draw_line_high(e, x1, y1, x0, y0);
+        } else {
+            draw_line_high(e, x0, y0, x1, y1);
+        }
     }
 }
 
@@ -52,7 +94,7 @@ fn main() {
             let y = y * f;
             points.push((cx+x, cy+y));
         }
-        draw_line(&mut easy, points[0], points[1]);
+        draw_line(&mut easy, points[0].0, points[0].1, points[1].0, points[1].1);
     }
 
     //loop {
