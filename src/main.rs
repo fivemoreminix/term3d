66b96bd0934eb8195515aef4de85bf2ba968d777
Luke Wilson <asmoaesl@gmail.com>
2018-11-28 18:08:32 -0600

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ extern crate easycurses;
 use easycurses::*;
 
 use std::thread::sleep;
-use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
 
-fn draw_cell(e: &mut EasyCurses, c:char,x:i32,y:i32) {
+fn draw_cell(e: &mut EasyCurses, c: char, x: i32, y: i32) {
     e.move_xy(x, y);
     e.print_char(c);
 }
 
-fn draw_line_low(e: &mut EasyCurses, x0:i32,y0:i32,x1:i32,y1:i32) {
+fn draw_line_low(e: &mut EasyCurses, x0: i32, y0: i32, x1: i32, y1: i32) {
     let dx = x1 - x0;
     let mut dy = y1 - y0;
     let mut yi = 1;
@@ -18,20 +18,20 @@ fn draw_line_low(e: &mut EasyCurses, x0:i32,y0:i32,x1:i32,y1:i32) {
         yi = -1;
         dy = -dy;
     }
-    let mut d = 2*dy - dx;
+    let mut d = 2 * dy - dx;
     let mut y = y0;
 
     for x in x0..x1 {
         draw_cell(e, '#', x, y);
         if d > 0 {
             y += yi;
-            d -= 2*dx;
+            d -= 2 * dx;
         }
-        d += 2*dy;
+        d += 2 * dy;
     }
 }
 
-fn draw_line_high(e: &mut EasyCurses, x0:i32,y0:i32,x1:i32,y1:i32) {
+fn draw_line_high(e: &mut EasyCurses, x0: i32, y0: i32, x1: i32, y1: i32) {
     let mut dx = x1 - x0;
     let dy = y1 - y0;
     let mut xi = 1;
@@ -39,20 +39,20 @@ fn draw_line_high(e: &mut EasyCurses, x0:i32,y0:i32,x1:i32,y1:i32) {
         xi = -1;
         dx = -dx;
     }
-    let mut d = 2*dx - dy;
+    let mut d = 2 * dx - dy;
     let mut x = x0;
 
     for y in y0..y1 {
         draw_cell(e, '#', x, y);
         if d > 0 {
             x += xi;
-            d -= 2*dy;
+            d -= 2 * dy;
         }
-        d += 2*dx;
+        d += 2 * dx;
     }
 }
 
-fn draw_line(e: &mut EasyCurses, x0:i32,y0:i32,x1:i32,y1:i32) {
+fn draw_line(e: &mut EasyCurses, x0: i32, y0: i32, x1: i32, y1: i32) {
     if (y1 - y0).abs() < (x1 - x0).abs() {
         if x0 > x1 {
             draw_line_low(e, x1, y1, x0, y0);
@@ -68,19 +68,19 @@ fn draw_line(e: &mut EasyCurses, x0:i32,y0:i32,x1:i32,y1:i32) {
     }
 }
 
-fn rotate_2d(pos: (f32,f32), rad: f32) -> (f32, f32) {
+fn rotate_2d(pos: (f32, f32), rad: f32) -> (f32, f32) {
     let (x, y) = pos;
     let (s, c) = (rad.sin(), rad.cos());
-    (x*c-y*s,y*c+x*s)
+    (x * c - y * s, y * c + x * s)
 }
 
 struct Camera {
-    pub pos: (f32,f32,f32),
-    pub rot: (f32,f32),
+    pub pos: (f32, f32, f32),
+    pub rot: (f32, f32),
 }
 
 impl Camera {
-    pub fn new(pos:(f32,f32,f32),rot:(f32,f32)) -> Camera {
+    pub fn new(pos: (f32, f32, f32), rot: (f32, f32)) -> Camera {
         Camera { pos, rot }
     }
 
@@ -92,13 +92,28 @@ impl Camera {
                 Input::Character('q') => self.pos.1 += s,
                 Input::Character('e') => self.pos.1 -= s,
 
-                Input::Character('w') | Input::Character('a') | Input::Character('s') | Input::Character('d') => {
-                    let (x, y) = (s*self.rot.1.sin(),s*self.rot.1.cos());
+                Input::Character('w')
+                | Input::Character('a')
+                | Input::Character('s')
+                | Input::Character('d') => {
+                    let (x, y) = (s * self.rot.1.sin(), s * self.rot.1.cos());
                     match input {
-                        Input::Character('w') => { self.pos.0 += x; self.pos.2 += y; }
-                        Input::Character('s') => { self.pos.0 -= x; self.pos.2 -= y; }
-                        Input::Character('a') => { self.pos.0 -= y; self.pos.2 += x; }
-                        Input::Character('d') => { self.pos.0 += y; self.pos.2 -= x; }
+                        Input::Character('w') => {
+                            self.pos.0 += x;
+                            self.pos.2 += y;
+                        }
+                        Input::Character('s') => {
+                            self.pos.0 -= x;
+                            self.pos.2 -= y;
+                        }
+                        Input::Character('a') => {
+                            self.pos.0 -= y;
+                            self.pos.2 += x;
+                        }
+                        Input::Character('d') => {
+                            self.pos.0 += y;
+                            self.pos.2 -= x;
+                        }
                         _ => unreachable!(),
                     }
                 }
@@ -125,15 +140,37 @@ fn main() {
 
     let frame_target_duration = Duration::new(1, 0).checked_div(60).unwrap();
 
-    let verts = [(-1.,-1.,-1.),(1.,-1.,-1.),(1.,1.,-1.),(-1.,1.,-1.),(-1.,-1.,1.),(1.,-1.,1.),(1.,1.,1.),(-1.,1.,1.)];
-    let edges = [(0.,1.),(1.,2.),(2.,3.),(3.,0.),(4.,5.),(5.,6.),(6.,7.),(7.,4.),(0.,4.),(1.,5.),(2.,6.),(3.,7.)];
+    let verts = [
+        (-1., -1., -1.),
+        (1., -1., -1.),
+        (1., 1., -1.),
+        (-1., 1., -1.),
+        (-1., -1., 1.),
+        (1., -1., 1.),
+        (1., 1., 1.),
+        (-1., 1., 1.),
+    ];
+    let edges = [
+        (0., 1.),
+        (1., 2.),
+        (2., 3.),
+        (3., 0.),
+        (4., 5.),
+        (5., 6.),
+        (6., 7.),
+        (7., 4.),
+        (0., 4.),
+        (1., 5.),
+        (2., 6.),
+        (3., 7.),
+    ];
 
     let (mut h, mut w) = easy.get_row_col_count();
     let (mut cx, mut cy) = (w as f32 / 2., h as f32 / 2.);
 
     let mut delta_time: f32 = 0.;
 
-    let mut cam = Camera::new((0.,0.,-5.),(0.,0.));
+    let mut cam = Camera::new((0., 0., -5.), (0., 0.));
 
     loop {
         let top_of_loop = Instant::now();
@@ -161,21 +198,27 @@ fn main() {
         }
 
         for edge in &edges {
-            let mut points: Vec<(f32,f32)> = Vec::new();
-            for (x,y,z) in &[verts[edge.0 as usize],verts[edge.1 as usize]] {
-                let mut x = x - cam.pos.0;
-                let mut y = y - cam.pos.1;
+            let mut points: Vec<(f32, f32)> = Vec::new();
+            for (x, y, z) in &[verts[edge.0 as usize], verts[edge.1 as usize]] {
+                let x = x - cam.pos.0;
+                let y = y - cam.pos.1;
                 let z = z - cam.pos.2;
 
-                let (mut x, z) = rotate_2d((x,z),cam.rot.1);
-                let (mut y, z) = rotate_2d((y,z),cam.rot.0);
+                let (mut x, z) = rotate_2d((x, z), cam.rot.1);
+                let (mut y, z) = rotate_2d((y, z), cam.rot.0);
 
-                let f = cx/z;
+                let f = cx / z;
                 x *= f;
                 y *= f;
-                points.push((cx+x, cy+y));
+                points.push((cx + x, cy + y));
             }
-            draw_line(&mut easy, points[0].0 as i32, points[0].1 as i32, points[1].0 as i32, points[1].1 as i32);
+            draw_line(
+                &mut easy,
+                points[0].0 as i32,
+                points[0].1 as i32,
+                points[1].0 as i32,
+                points[1].1 as i32,
+            );
         }
 
         let elapsed_this_frame = top_of_loop.elapsed();
@@ -186,7 +229,7 @@ fn main() {
         easy.refresh();
 
         let elapsed_after_updates = after_updates.elapsed();
-        delta_time = (elapsed_after_updates.as_secs() as f32) + ((elapsed_after_updates.subsec_nanos() as f32) / 1000000000.0);
+        delta_time = (elapsed_after_updates.as_secs() as f32)
+            + ((elapsed_after_updates.subsec_nanos() as f32) / 1000000000.0);
     }
 }
-
